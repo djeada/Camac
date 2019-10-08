463b02475045f9b74d061c930b0e7a5f22d150b3
Adam Djellouli <01102712@pw.edu.pl>
2019-10-08 22:03:00 +0200

    Maxwell zostanie przyćmiony

--- Algorytmy_Sortowania.py
+++ Algorytmy_Sortowania.py
@@ -1,11 +1,32 @@
 import random
 import time
+import pygame
+import winsound
+from pygame.locals import *
 
-def sortowanie_babelkowe(tablica):
-    for i in range(len(tablica)):
-        for j in range(len(tablica)):
-            if tablica[i] > tablica[j]:
-                swap(i, j, tablica)
+def setup():
+    pygame.init()
+    pygame.display.set_caption("Algorytmy Sortowania")
+    window = pygame.display.set_mode(((1000,800)))
+    window.set_alpha(None)
+    window.fill(pygame.Color(0,0,0))
+    return window
+
+def draw(tab, szerokosci, window):
+    window.fill(pygame.Color(0,0,0))
+    for i in range(len(tab)):
+        pygame.draw.rect(window,pygame.Color(255,255,255),(szerokosci[i],800-tab[i],10,tab[i]),0)
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONUP:
+            None
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    pygame.display.update()
+
+def swap(i,j, tablica):
+    temp = tablica[i]
+    tablica[i] = tablica[j]
+    tablica[j] = temp
 
 def znajdzMin(tablica):
     minimum = tablica[0]
@@ -16,42 +37,72 @@ def znajdzMin(tablica):
             indeks = i
     return indeks
 
-def przez_wybieranie(tablica):
+def sortowanie_babelkowe(tablica, szerokosci, window):
+    for i in range(len(tablica)):
+        for j in range(len(tablica)):
+            if tablica[i] < tablica[j]:
+                swap(i, j, tablica)
+                winsound.Beep(tablica[j] + 2000,10)
+            draw(tablica, szerokosci, window)
+
+def sortowanie_wybieranie(tablica, szerokosci, window):
     for i in range(len(tablica)):
         indeks = znajdzMin(tablica[i:len(tablica)]) + i
         swap(i,indeks,tablica)
+        draw(tablica, szerokosci, window)
+        winsound.Beep(tablica[i] + 2000,10)
 
-def sortowanie_szybkie(tablica):
+
+def sortowanie_szybkie(tablica, szerokosci, window):
     if len(tablica) > 1:
-        return sortowanie_szybkie([x for x in tablica[1:] if x < tablica[0]]) + [x for x in tablica if x == tablica[0]] + sortowanie_szybkie([x for x in tablica[1:] if x > tablica[0]])
+        draw(tablica, szerokosci, window)
+        winsound.Beep(random.randint(0,100) + 2000,10)    
+        return sortowanie_szybkie([x for x in tablica[1:] if x < tablica[0]], szerokosci, window) + [x for x in tablica if x == tablica[0]] + sortowanie_szybkie([x for x in tablica[1:] if x > tablica[0]], szerokosci, window)
     else:
         return tablica
-    
-def swap(i,j, tablica):
-    temp = tablica[i]
-    tablica[i] = tablica[j]
-    tablica[j] = temp
 
-tab = [[],[],[]]
-czas = []
+def wypisz_slownik(slownik):
+    for key in slownik:
+        print(key, " ", slownik[key])
 
-for i in range(10000):
-    losowa = random.randint(0,10000)
-    tab[0].append(losowa)
-    tab[1].append(losowa)
-    tab[2].append(losowa)
+tab = []
+czas = {}
+szerokosci = []
+szerokosc = 0
 
+for i in range(100):
+    losowa = random.randint(0,800)
+    tab.append(losowa)
+    szerokosci.append(szerokosc)
+    szerokosc += 10
 
-start_time = time.time()
-sortowanie_babelkowe(tab[0])
-czas.append(time.time() - start_time)
+window = setup()
 
-start_time = time.time()
-przez_wybieranie(tab[1])
-czas.append(time.time() - start_time)
+def menu(tab, szerokosci, window, czas):
+    while True:
+        print("Wybierz sortowanie:")
+        print("1. Babelkowe")
+        print("2. Wybieranie")
+        print("3. Szybkie")
+        print("4. Zliczanie")
+        print("5. Kopcowanie")
+        print("6. Pozycjne")
+        print("7. Porownaj czas dla roznych sortowan")
+        wybor = int(input())
+        if(wybor == 1):
+            start_time = time.time()
+            sortowanie_babelkowe(tab, szerokosci, window)
+            czas["babelkowe"] = time.time() - start_time
+        if(wybor == 2):
+            start_time = time.time()
+            sortowanie_wybieranie(tab, szerokosci, window)
+            czas["wybieranie"] = time.time() - start_time
+        if(wybor == 3):
+            start_time = time.time()
+            sortowanie_szybkie(tab, szerokosci, window)
+            czas["szybkie"] = time.time() - start_time
+        if(wybor == 7):
+            wypisz_slownik(czas)
 
-start_time = time.time()
-tab = sortowanie_szybkie(tab[2])
-czas.append(time.time() - start_time)
-
-print(czas)
+menu(tab, szerokosci, window, czas)
+            
