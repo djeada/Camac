8334f9a1dd1112c4dd6e43bde27ba991086254ea
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-10-11 17:48:25 +0200

    Add files via upload

--- Lekcja2/Lista.py
+++ Lekcja2/Lista.py
@@ -15,6 +15,7 @@ class Lista():
             while licznik.nastepny != None:
                 licznik = licznik.nastepny
             licznik.nastepny = Pudelko(dane)
+
     def wyswietl(self):
         if self.glowa.dane == None:
             print('Lista jest pusta')
@@ -24,6 +25,7 @@ class Lista():
                 print(licznik.dane)
                 licznik = licznik.nastepny
             print(licznik.dane)
+
     def dlugosc(self):
         dlugosc = 0
         if self.glowa.dane != None:
@@ -33,6 +35,7 @@ class Lista():
                 licznik = licznik.nastepny
                 dlugosc += 1
         return dlugosc
+
     def usun_ostatni(self):
         if self.glowa.dane != None:
             licznik = self.glowa
@@ -73,6 +76,20 @@ class Lista():
                     return
                 pozycja += 1
             licznik.nastepny = None
+    def pobierz(self, i):
+        if self.glowa.dane == None:
+            print('lista jest pusta!')
+            return
+        if i >= self.dlugosc() or i < 0:
+            print('zly indeks')
+            return
+        licznik = self.glowa
+        pozycja = 0
+        while licznik:
+            if pozycja == i:
+                return licznik.dane
+            pozycja += 1
+            licznik = licznik.nastepny
 
     def znajdzNajkrotsza(self):
         if self.glowa.dane == None:
@@ -89,6 +106,75 @@ class Lista():
                 najkrotszy = licznik
         return najkrotszy.dane
 
+    def sortuj(self):
+        print('Wybierz rodzaj sortowania')
+        print('1. Alfabetycznie')
+        print('2. Wzgledem ilosci stron')
+        wybor = int(input())
+        if wybor == 1:
+            return self.sortuj_przez_scalanie(0)
+        if wybor == 2:
+            return self.sortuj_przez_scalanie(1)
+
+    def podziel(self):
+        dlugosc = self.dlugosc()
+        if dlugosc == 0:
+            return None
+        if dlugosc == 1:
+            return self.glowa
+
+        a = Lista()
+        b = Lista()
+        
+        srodek = int(dlugosc/2)
+        licznik = self.glowa
+        pozycja = 0
+
+        while pozycja < srodek:
+            a.dodaj(licznik.dane)
+            licznik = licznik.nastepny
+            pozycja += 1
+
+        while licznik:
+            b.dodaj(licznik.dane)
+            licznik = licznik.nastepny
+        
+        return a,b
+
+    def sortuj_przez_scalanie(self, x):
+        if self.dlugosc() <= 1:
+            return self
+        else:
+            a, b = self.podziel()
+            return scal(a.sortuj_przez_scalanie(x), b.sortuj_przez_scalanie(x), x)
+ 
+
+def scal(a,b,x):
+    pozycja_a = pozycja_b = 0
+
+    polaczone_listy = Lista()
+    dlugosc_poloczonych_list = a.dlugosc() + b.dlugosc()
+
+    while polaczone_listy.dlugosc() < dlugosc_poloczonych_list:
+        if a.pobierz(pozycja_a)[x] <= b.pobierz(pozycja_b)[x]:
+            polaczone_listy.dodaj(a.pobierz(pozycja_a))
+            pozycja_a += 1
+        else:
+            polaczone_listy.dodaj(b.pobierz(pozycja_b))
+            pozycja_b += 1
+
+        if pozycja_a == a.dlugosc():
+            while pozycja_b < b.dlugosc():
+                polaczone_listy.dodaj(b.pobierz(pozycja_b))
+                pozycja_b += 1
+
+        elif pozycja_b == b.dlugosc():
+            while pozycja_a < a.dlugosc():
+                polaczone_listy.dodaj(a.pobierz(pozycja_a))
+                pozycja_a += 1
+                
+    return polaczone_listy
+
 listaA = Lista()
 listaA.dodaj(('Pan Tadeusz', 340))
 listaA.dodaj(('Pan Amadeusz', 843))
@@ -96,6 +182,6 @@ listaA.dodaj(('Pan Maxwell', 143))
 listaA.dodaj(('Pan Tadeusz', 356))
 listaA.usun_ostatni()
 listaA.wstaw(('Hej',33), 1)
-listaA.wyswietl()
+listaA.sortuj().wyswietl()
 print('Najkrotsza ksiazka to ', listaA.znajdzNajkrotsza())
 print(listaA.dlugosc())
