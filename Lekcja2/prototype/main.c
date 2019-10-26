7fd3ae810450deaa6bd436f61b0c2f0492862386
Bartłomiej Kliś <36824240+barklis@users.noreply.github.com>
2019-10-26 23:17:38 +0200

    Update main.c
    Z laweczki szybki update

--- Lekcja2/prototype/main.c
+++ Lekcja2/prototype/main.c
@@ -5,28 +5,28 @@ int main()
 	char a = 'A';
 	struct List* l;
 	init(l, sizeof(char));
-	push(l, (void*) &a);
+	push_list(l, (void*) &a);
 	a = 'b';
-	push(l, (void*) &a);
+	push_list(l, (void*) &a);
 	a = 'c';
-	push(l, (void*) &a);
+	push_list(l, (void*) &a);
 	a = 'D';
-	insert(l, (void*) &a, 0);
+	insert_list(l, (void*) &a, 0);
 
-	printf("Dana z listy %c\n", *((char*) get(l, 0)));
-	printf("Dana z listy %c\n", *((char*) get(l, 1)));
-	printf("Dana z listy %c\n", *((char*) get(l, 2)));
-	printf("Dana z listy %c\n", *((char*) get(l, 3)));
+	printf("Dana z listy %c\n", *((char*) get_list(l, 0)));
+	printf("Dana z listy %c\n", *((char*) get_list(l, 1)));
+	printf("Dana z listy %c\n", *((char*) get_list(l, 2)));
+	printf("Dana z listy %c\n", *((char*) get_list(l, 3)));
 	printf("Dlugosc: %ld\n", l->num);
 
 	print_data(l, 2, char, "Wartosc: %c\n");
-	del(l, 1);
+	del_list(l, 1);
 	print_all(l, char, "Wartosc: %c\n");
 
-	pop(l);
-	pop(l);
+	pop_list(l);
+	pop_list(l);
 	printf("Dlugosc: %ld\n", l->num);
-	clear(l);
+	clear_list(l);
 	free_list(l);
 
 	return 0;
