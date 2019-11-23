7a7af018b19d9b2718ba53ec08111e000e109381
BorysPcki <37407594+BorysPcki@users.noreply.github.com>
2019-11-23 16:53:45 +0100

    teraz z wyszukiwaniem

--- Lekcja4/BinaryTree/src/com/company/BinaryTree.java
+++ Lekcja4/BinaryTree/src/com/company/BinaryTree.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.util.Random;
+import java.util.stream.IntStream;
+
 class Nod {
 
     Integer data;
@@ -38,7 +41,7 @@ class Nod {
 class BinaryTree {
     private Nod root;
 
-    BinaryTree(Nod givenRoot) {
+    private BinaryTree(Nod givenRoot) {
         root = givenRoot;
     }
 
@@ -56,49 +59,76 @@ class BinaryTree {
         }
     }
 
-    void add(int data) {
+    private void add(int data) {
         if (root.data == null)
             root.data = data;
         else
             addToNod(root, data);
     }
 
-    void preorder(Nod givenRoot) {
+    private void preorder(Nod givenRoot) {
         if (givenRoot == null) return;
-        System.out.println(givenRoot.data);
+        System.out.print(givenRoot.data + "-");
         preorder(givenRoot.left);
         preorder(givenRoot.right);
     }
 
-    void inorder(Nod givenRoot) {
-        if(givenRoot == null) return;
+    private void inorder(Nod givenRoot) {
+        if (givenRoot == null) return;
         inorder(givenRoot.left);
-        System.out.println(givenRoot.data);
+        System.out.print(givenRoot.data + "-");
         inorder(givenRoot.right);
     }
 
-    void postorder(Nod givenRoot) {
-        if(givenRoot == null) return;
+    private void postorder(Nod givenRoot) {
+        if (givenRoot == null) return;
         postorder(givenRoot.left);
         postorder(givenRoot.right);
-        System.out.println(givenRoot.data);
+        System.out.print(givenRoot.data + "-");
+    }
+
+    private Nod searchNod(Nod nod, int data) {
+        Nod tmp = null;
+        if (nod != null) {
+            if (nod.data == data)
+                tmp = nod;
+            else if (data < nod.data)
+                tmp = searchNod(nod.left, data);
+            else
+                tmp = searchNod(nod.right, data);
+        }
+        return tmp;
+    }
+
+    private Nod search(int data) {
+        if (root.data == data)
+            return root;
+        return searchNod(root, data);
     }
 
     public static void main(String[] args) {
-        Nod root = new Nod(10);
+        Nod root = new Nod(50);
         BinaryTree binaryTree = new BinaryTree(root);
-        binaryTree.add(5);
-        binaryTree.add(3);
-        binaryTree.add(7);
-        binaryTree.add(2);
-        binaryTree.add(12);
+
+        Random random = new Random();
+        IntStream.range(0, 100).parallel().forEach(
+                nbr -> binaryTree.add(random.nextInt(100))
+        );
+
+
         binaryTree.preorder(root);
         System.out.println();
         binaryTree.inorder(root);
         System.out.println();
         binaryTree.postorder(root);
+        System.out.println();
+
+        int searchFor = 7;
+        try {
+            Nod nod = binaryTree.search(searchFor);
+            System.out.println("Found node: " + nod.data);
+        } catch (NullPointerException e) {
+            System.out.println("Not found node: " + searchFor);
+        }
     }
 }
-
-
-
